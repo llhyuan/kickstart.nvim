86cb486f5eed455100afd9d74dc045f4dca54112
hangyuan <hangyuan@flowplan.dk>
2025-06-07 09:08:45 +0200

    try config organize import for ts/js projects

--- init.lua
+++ init.lua
@@ -360,7 +360,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -403,7 +403,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
         --{ '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
@@ -443,7 +443,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -571,7 +571,7 @@ require('lazy').setup({
       },
     },
   },
-  { 'Bilal2453/luvit-meta',                        lazy = true },
+  { 'Bilal2453/luvit-meta', lazy = true },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -583,7 +583,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       opts = {} },
+      { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -705,6 +705,30 @@ require('lazy').setup({
               buffer = event.buf,
               callback = function()
                 vim.lsp.buf.format { bufnr = event.buf, id = client.id }
+              end,
+            })
+          end
+
+          if client and client.name == 'vtsls' then
+            local function organize_imports()
+              local ft = vim.bo.filetype:gsub('react$', '')
+              if not vim.tbl_contains({ 'javascript', 'typescript' }, ft) then
+                return
+              end
+              local ok = vim.lsp.buf_request_sync(0, 'workspace/executeCommand', {
+                command = (ft .. '.organizeImports'),
+                arguments = { vim.api.nvim_buf_get_name(0) },
+              }, 3000)
+              if not ok then
+                print 'Command timeout or failed to complete.'
+              end
+            end
+
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              pattern = { '*.css', '*.html', '*.js', '*.jsx', '*.json', '*.ts', '*.tsx' },
+              callback = function()
+                require('conform').format { async = false }
+                organize_imports()
               end,
             })
           end
@@ -848,8 +872,8 @@ require('lazy').setup({
       local opts = {
         default_format_opts = {
           timeout_ms = 3000,
-          async = false,           -- not recommended to change
-          quiet = false,           -- not recommended to change
+          async = false, -- not recommended to change
+          quiet = false, -- not recommended to change
           lsp_format = 'fallback', -- not recommended to change
         },
         format_on_save = function(bufnr)
@@ -1192,8 +1216,7 @@ require('lazy').setup({
             command = 'node',
             -- 💀 Make sure to update this path to point to your installation
             args = {
-              require('mason-registry').get_package('js-debug-adapter'):get_install_path() ..
-              '/js-debug/src/dapDebugServer.js',
+              require('mason-registry').get_package('js-debug-adapter'):get_install_path() .. '/js-debug/src/dapDebugServer.js',
               '${port}',
             },
           },
@@ -1264,8 +1287,8 @@ require('lazy').setup({
     'rebelot/kanagawa.nvim',
     lazy = false,
     opts = {
-      theme = 'wave',  -- Load "wave" theme
-      background = {   -- map the value of 'background' option to a theme
+      theme = 'wave', -- Load "wave" theme
+      background = { -- map the value of 'background' option to a theme
         dark = 'wave', -- try "dragon" !
         light = 'lotus',
       },
@@ -1279,7 +1302,7 @@ require('lazy').setup({
     --   vim.cmd 'colorscheme rose-pine'
     -- end,
   },
-  { 'sainnhe/sonokai',        lazy = true },
+  { 'sainnhe/sonokai', lazy = true },
   {
     'sainnhe/everforest',
     lazy = true,
@@ -1696,12 +1719,12 @@ require('lazy').setup({
       },
     },
     keys = {
-      { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>',              desc = 'Diagnostics (Trouble)' },
+      { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (Trouble)' },
       { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Buffer Diagnostics (Trouble)' },
-      { '<leader>cs', '<cmd>Trouble symbols toggle<cr>',                  desc = 'Symbols (Trouble)' },
-      { '<leader>cS', '<cmd>Trouble lsp toggle<cr>',                      desc = 'LSP references/definitions/... (Trouble)' },
-      { '<leader>xL', '<cmd>Trouble loclist toggle<cr>',                  desc = 'Location List (Trouble)' },
-      { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>',                   desc = 'Quickfix List (Trouble)' },
+      { '<leader>cs', '<cmd>Trouble symbols toggle<cr>', desc = 'Symbols (Trouble)' },
+      { '<leader>cS', '<cmd>Trouble lsp toggle<cr>', desc = 'LSP references/definitions/... (Trouble)' },
+      { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
+      { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
       {
         '[q',
         function()
@@ -1752,7 +1775,7 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim',       event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
@@ -1848,7 +1871,7 @@ require('lazy').setup({
           -- mapping query_strings to modes.
           selection_modes = {
             ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V',  -- linewise
+            ['@function.outer'] = 'V', -- linewise
             ['@class.outer'] = '<c-v>', -- blockwise
           },
           -- If you set this to `true` (default is `false`) then any textobject is
@@ -1904,7 +1927,7 @@ require('lazy').setup({
             -- mapping query_strings to modes.
             selection_modes = {
               ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V',  -- linewise
+              ['@function.outer'] = 'V', -- linewise
               ['@class.outer'] = '<c-v>', -- blockwise
             },
             -- If you set this to `true` (default is `false`) then any textobject is
