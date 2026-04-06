799eec4ed78285171cbffd56df4d6c77d260d6dc
hangyuan <hangyuan@flowplan.dk>
2026-04-06 15:23:28 +0200

    add border to float window

--- init.lua
+++ init.lua
@@ -718,6 +718,11 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
+          -- Open hover documentation with padding
+          map('K', function()
+            vim.lsp.buf.hover { border = 'single', padding = { 1, 2 } }
+          end, '[K] Hover Documentation')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
